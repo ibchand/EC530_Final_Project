cd1b0db9774943bcc236b116319e6c8ab93b0840
ibchand <ichand@bu.edu>
2022-05-08 20:21:19 -0400

    Removed google maps import

--- TreeFinder/TreeFinder/settings.py
+++ TreeFinder/TreeFinder/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', 
-    'django_google_maps',
+    # 'django_google_maps',
     'django_extensions',
 ]
 
